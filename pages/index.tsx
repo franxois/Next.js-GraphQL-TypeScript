7cdd7529bee1ff01dd520abd93788045bf26ed6d
François Rieucau <franxoois@gmail.com>
2018-04-14 22:35:59 +0200

    build(typescript): Add typescript and change index.js to index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default class App extends React.Component<any, any> {
+    render() {
+        return (
+            <div>Hello from next.js !</div>
+        );
+    }
+}
