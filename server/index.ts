5afeb06aa9a49ddf474a0dbc647fb89a635658fe
François Rieucau <frieucau@fastbooking.net>
2018-04-25 17:23:00 +0200

    feat(graphql): Add custom apollo-server-express server and /graphiql route
    Inspired by https://github.com/zeit/next.js/tree/canary/examples/custom-server-typescript and
    https://github.com/zeit/next.js/tree/canary/examples/custom-server-express

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import * as express from "express";
+import { graphiqlExpress, graphqlExpress } from "apollo-server-express";
+import * as next from "next";
+
+const port = parseInt(process.env.PORT, 10) || 4011;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server: express.Application = express();
+
+  if (process.env.NODE_ENV !== "production") {
+    server.get("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+  }
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
