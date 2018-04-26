033f1bdc333816acb51d936b525af6771f4ede31
François Rieucau <frieucau@fastbooking.net>
2018-04-26 19:19:55 +0200

    fix(storybook): Make storybook working
    Use jsw:react instead of jsx:preserve and import * as React from "react"; instead of import {
    Component } from "react";

--- components/SampleWidget.tsx
+++ components/SampleWidget.tsx
@@ -1,10 +1,9 @@
-import { Component } from "react";
-
+import * as React from "react";
 export interface SampleWidgetProps {
   name: string;
 }
 
-export class SampleWidget extends Component<SampleWidgetProps, {}> {
+export class SampleWidget extends React.Component<SampleWidgetProps, {}> {
   render() {
     return (
       <div>
