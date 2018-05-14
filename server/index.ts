77c3bd6aed4089f585ce5db2b310263cee59dbca
François Rieucau <frieucau@fastbooking.net>
2018-05-14 17:21:32 +0200

    test(graphiql): Write graphiql url at start

--- server/index.ts
+++ server/index.ts
@@ -20,6 +20,8 @@ app.prepare().then(() => {
 
   server.listen(port, err => {
     if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log(
+      `> Ready on http://localhost:${port} & http://localhost:${port}/graphiql`
+    );
   });
 });
